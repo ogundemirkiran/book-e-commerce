20f0c0a7efc62c2866eb0bdb853f8968b61a23da
Ogün Demirkıran <demirkiranogun@gmail.com>
2024-08-16 17:06:11 +0300

    updated

--- pages/shopping-cart/index.vue
+++ pages/shopping-cart/index.vue
@@ -30,7 +30,7 @@
           <nuxt-link to="/">
             <button
               type="button"
-              class="mt-2 text-xs text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              class="mt-2 text-xs text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 text-center me-2 mb-2"
             >
               Continue Shopping
             </button>
