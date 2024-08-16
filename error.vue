1546532a51e7c3e7fc0b2d870243beabe531efbc
Ogün Demirkıran <demirkiranogun@gmail.com>
2024-08-16 16:47:14 +0300

    added error page

---
+++ error.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    v-if="error.statusCode === 404"
+    class="flex flex-col items-center justify-center h-screen text-center bg-gray-100"
+  >
+    <img
+      src="@/assets/images/notFound.png"
+      class="inline me-3"
+      alt="Complete"
+    />
+    <p class="mt-4 text-xl text-gray-700">
+      Oops! The page you're looking for doesn't exist.
+    </p>
+    <nuxt-link
+      to="/"
+      class="mt-6 px-6 py-2 bg-purple-600 text-white rounded-lg hover:bg-purple-700"
+    >
+      Go Back Home
+    </nuxt-link>
+  </div>
+  <div v-else>
+    <h1 class="text-center">{{ error.statusCode }} - {{ error.message }}</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  error: {
+    type: Object,
+    default: () => ({ statusCode: 404, message: 'Page not found' })
+  }
+});
+</script>
--- pages/completed/index.vue
+++ pages/completed/index.vue
@@ -17,7 +17,7 @@
     <nuxt-link to="/" class="mt-5">
       <button
         type="button"
-        class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
       >
         Continue Shopping
       </button>
